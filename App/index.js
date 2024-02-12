6c7ec611ed8682f485a45e5cd00c22aa94616e7e
XietrZ <sxietrz@yahoo.com>
2024-02-12 22:28:17 +0800

    Initialize

---
+++ App/index.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/style-prop-object */
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Open up App.js to start working on your app! Paul Alvin V. Sacedor Love
+        Dianne Joy Adobo
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
